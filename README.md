bde2d5e1ebfc323e04ae696f56fde257da00b021
abhinavraj5446 <101207351+abhinavraj5446@users.noreply.github.com>
2023-08-10 01:44:15 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# personal-expense
+#this-project-store-the-data-of-daily-expense
+
+# personal-expense
+# personal-expense
+# personal-kharcha
+# personal-kharcha
+# khata-kharcha
---
+++ intro.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Button click event to navigate to main tracker page
+    var startBtn = document.getElementById('strt-btn');
+    startBtn.addEventListener('click', function() {
+      window.location.href = 'expense.html';
+    });
+  });
+  
